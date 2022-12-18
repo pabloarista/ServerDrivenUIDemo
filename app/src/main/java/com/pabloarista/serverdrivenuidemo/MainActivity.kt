383effcdc5c1210fbc241ad3c883ee1468b88ed9
pqsk <3964831+pabloarista@users.noreply.github.com>
2022-12-18 03:22:47 -0500

    updated function call from build to Build

--- app/src/main/java/com/pabloarista/serverdrivenuidemo/MainActivity.kt
+++ app/src/main/java/com/pabloarista/serverdrivenuidemo/MainActivity.kt
@@ -13,10 +13,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,17 +26,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontSynthesis
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.pabloarista.serverdrivenuidemo.ui.build
+import com.pabloarista.serverdrivenuidemo.ui.Build
 import com.pabloarista.serverdrivenuidemo.ui.theme.ServerDrivenUIDemoTheme
 import com.pabloarista.serverdrivenuidemo.ui.toColor
 import org.simpleframework.xml.Serializer
@@ -57,7 +52,7 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun serverContent(component: Component) {
-        component.build()
+        component.Build()
     }
 
     fun serverContentCallback(component: Component?) {
