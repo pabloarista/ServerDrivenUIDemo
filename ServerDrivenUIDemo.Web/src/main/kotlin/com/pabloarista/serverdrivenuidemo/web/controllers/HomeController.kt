81660bdcdae4e4ccdae4f276690f27598941af15
pqsk <3964831+pabloarista@users.noreply.github.com>
2022-12-18 03:21:02 -0500

    added rest api that loads an xml file and then responds as json

--- ServerDrivenUIDemo.Web/src/main/kotlin/com/pabloarista/serverdrivenuidemo/web/controllers/HomeController.kt
+++ ServerDrivenUIDemo.Web/src/main/kotlin/com/pabloarista/serverdrivenuidemo/web/controllers/HomeController.kt
@@ -1,4 +1,36 @@
 package com.pabloarista.serverdrivenuidemo.web.controllers
 
+import com.pabloarista.serverdrivenuidemo.shared.data.models.Component
+import org.simpleframework.xml.Serializer
+import org.simpleframework.xml.core.Persister
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.*
+
+
+@RestController
 class HomeController {
+    @GetMapping("/")
+    fun index(): Component? {
+        var component: Component?
+        try {
+            val rootPath = File("").absoluteFile
+            //instead of grabbing it from the resource file we grab it directly from the file system.
+            // This way the file can be modified and the web server does NOT need to be restarted.
+            val completePath = "$rootPath/ServerDrivenUIDemo.Web/src/main/resources/test.xml"
+            FileInputStream(completePath).use { inputStream ->
+                val serializer: Serializer = Persister()
+                component = serializer.read(Component::class.java, inputStream)
+            }
+        } catch (ex: IOException) {
+            println(ex)
+            component = null
+        }
+        return component
+    }
+
+    @GetMapping("/test")
+    fun test(): Component {
+        return Component.testData()
+    }
 }
